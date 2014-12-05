a069df8b595e2b99359f01539fe0441f05e75d9a
herve.tutuaku <herve.tutuaku@nerd.nintendo.com>
2014-12-05 14:49:42 +0100

    Translation-ja.js

---
+++ translation-ja.js
@@ -0,0 +1,273 @@
+var translate = function(){
+		var w="";
+		var tw="";
+		var key;
+};
+
+translate.jp = {
+
+	a : [
+	["a","あ","ア"],
+	["i","い","イ"],
+	["u","う","ウ"],
+	["e","え","エ"],
+	["o","お","オ"],
+	["n","ん","ン"],
+	["ka","か","カ"],
+	["ki","き","キ"],
+	["ku","く","ク"],
+	["ke","け","ケ"],
+	["ko","こ","コ"],
+	["sa","さ","サ"],
+	["shi","し","シ"],
+	["si","し","シ"],
+	["su","す","ス"],
+	["se","せ","セ"],
+	["so","そ","ソ"],
+	["ta","た","タ"],
+	["chi","ち","チ"],
+	["tsu","つ","ツ"],
+	["te","て","テ"],
+	["to","と","ト"],
+	["na","な","ナ"],
+	["ni","に","ニ"],
+	["nu","ぬ","ヌ"],
+	["ne","ね","ネ"],
+	["no","の","ノ"],
+	["ha","は","ハ"],
+	["hi","ひ","ヒ"],
+	["fu","ふ","フ"],
+	["he","へ","ヘ"],
+	["ho","ほ","ホ"],
+	["ma","ま","マ"],
+	["mi","み","ミ"],
+	["mu","む","ム"],
+	["me","め","メ"],
+	["mo","も","モ"],
+	["ya","や","ヤ"],
+	["yu","ゆ","ユ"],
+	["yo","よ","ヨ"],
+	["ra","ら","ラ"],
+	["ri","り","リ"],
+	["ru","る","ル"],
+	["re","れ","レ"],
+	["ro","ろ","ロ"],
+	["wa","わ","ワ","ウァ","うぁ"],
+	["ga","が","ガ"],
+	["gi","ぎ","ギ"],
+	["gu","ぐ","グ"],
+	["ge","げ","ゲ"],
+	["go","ご","ゴ"],
+	["za","ざ","ザ"],
+	["ji","じ","ジ"],
+	["zi","じ","ジ"],
+	["zu","ず","ズ"],
+	["ze","ぜ","ゼ"],
+	["zo","ぞ","ゾ"],
+	["da","だ","ダ"],
+	["di","ぢ","ヂ","でぃ","ディ"],
+	["du","づ","ヅ"],
+	["de","で","デ"],
+	["do","ど","ド"],
+	["ba","ば","バ"],
+	["bi","び","ビ"],
+	["bu","ぶ","ブ"],
+	["be","べ","ベ"],
+	["bo","ぼ","ボ"],
+	["pa","ぱ","パ"],
+	["pi","ぴ","ピ"],
+	["pu","ぷ","プ"],
+	["pe","ぺ","ペ"],
+	["po","ぽ","ポ"],
+	["kya","きゃ","キャ"],
+	["kyu","きゅ","キュ"],
+	["kyo","きょ","キョ"],
+	["sha","しゃ","シャ"],
+	["shu","しゅ","シュ"],
+	["sho","しょ","ショ"],
+	["sya","しゃ","シャ"],
+	["syu","しゅ","シュ"],
+	["syo","しょ","ショ"],
+	["cha","ちゃ","チャ"],
+	["chu","ちゅ","チュ"],
+	["cho","ちょ","チョ"],
+	["tya","ちゃ","チャ"],
+	["tyo","ちょ","チョ"],
+	["nya","にゃ","ニャ"],
+	["nyu","にゅ","ニュ"],
+	["nyo","にょ","ニョ"],
+	["hya","ひゃ","ヒャ"],
+	["hyu","ひゅ","ヒュ"],
+	["hyo","ひょ","ヒョ"],
+	["mya","みゃ","ミャ"],
+	["myu","みゅ","ミュ"],
+	["myo","みょ","ミョ"],
+	["rya","りゃ","リャ"],
+	["ryu","りゅ","リュ"],
+	["ryo","りょ","リョ"],
+	["gya","ぎゃ","ギャ"],
+	["gyu","ぎゅ","ギュ"],
+	["gyo","ぎょ","ギョ"],
+	["ja","じゃ","ジャ","ぢゃ","ヂャ"],
+	["ju","じゅ","ジュ","ぢゅ","ヂュ"],
+	["jo","じょ","ジョ","ぢょ","ヂョ"],
+	["zya","じゃ","ジャ"],
+	["zyu","じゅ","ジュ"],
+	["zyo","じょ","ジョ"],
+	["dya","ぢゃ","ヂャ"],
+	["dyu","ぢゅ","ヂュ"],
+	["dyo","ぢょ","ヂョ"],
+	["bya","びゃ","ビャ"],
+	["byu","びゅ","ビュ"],
+	["byo","びょ","ビョ"],
+	["pya","ぴゃ","ピャ"],
+	["pyu","ぴゅ","ピュ"],
+	["pyo","ぴょ","ピョ"],
+	["ye","いぇ","イェ"],
+	["we","うぇ","ウェ","ヱ"],
+	["wi","うぃ","ウィ","ヰ"],
+	["wo","ウォ","うぉ"],
+	["kwa","くぁ","クァ"],
+	["kwi","くぃ","クィ"],
+	["kwe","くぇ","クェ"],
+	["kwo","くぉ","クォ"],
+	["kwa","くぁ","クァ","くゎ","クヮ"],
+	["she","しぇ","シェ"],
+	["che","ちぇ","チェ"],
+	["tsa","つぁ","ツァ"],
+	["tsi","つぃ","ツィ"],
+	["tse","つぇ","ツェ"],
+	["tso","つぉ","ツォ"],
+	["tsyu","つゅ","ツュ"],
+	["ti","てぃ","ティ","ち","チ"],
+	["tyu","てゅ","テュ","ちゅ","チュ"],
+	["tu","とぅ","トゥ","つ","ツ"],
+	["nye","にぇ","ニェ"],
+	["hye","ひぇ","ヒェ"],
+	["hu","ふ","フ","ホゥ"],
+	["fa","ふぁ","ファ"],
+	["fi","ふぃ","フィ"],
+	["fe","ふぇ","フェ"],
+	["fo","ふぉ","フォ"],
+	["fye","ふぃぇ","フィェ"],
+	["fya","ふゃ","フャ"],
+	["fyu","ふゅ","フュ"],
+	["fyo","ふょ","フョ"],
+	["mye","みぇ","ミェ"],
+	["rye","りぇ","リェ"],
+	["vu","ゔ","ヴ"],
+	["va","ゔぁ","ヴァ","ヷ"],
+	["vi","ゔぃ","ヴィ","ヸ"],
+	["ve","ゔぇ","ヴェ","ヹ"],
+	["vo","ゔぉ","ヴォ","ヺ"],
+	["vye","ゔぃぇ","ヴィェ"],
+	["vya","ゔゃ","ヴャ"],
+	["vyu","ゔゅ","ヴュ"],
+	["vyo","ゔょ","ヴョ"],
+	["gye","ぎぇ","ギェ"],
+	["gwa","ぐぁ","グァ","げょ","グヮ"],
+	["gwi","ぐぃ","グィ"],
+	["gwe","ぐぇ","グェ"],
+	["gwo","ぐぉ","グォ"],
+	["geo","げぉ","ゲォ"],
+	["geyo","げょ","ゲョ"],
+	["je","じぇ","ジェ"],
+	["dyu","でゅ","デュ"],
+	["du","どぅ","ドゥ","ず","ズ"],
+	["bye","びぇ","ビェ"],
+	["pye","ぴぇ","ピェ"]],
+
+	vowel : ["a","e","i","o","u"],
+
+	instant_translation : function(e, txt){
+				key = String.fromCharCode(e.keyCode).toLowerCase();
+				var a = translate.jp.a;
+				var v = translate.jp.vowel;
+				var k = txt;
+				var hk = $('#trlt_htxt').val();
+				w += key;
+
+				var matches = new Array();
+				var nb = 0;
+
+				var idx = w.lastIndexOf(key);
+				var tln = k.length;
+
+				$('#trlt_htxt').val(w);
+
+				for(key1 in a){ // First loop
+					var jp_char = new RegExp("("+a[key1][0]+")", "i");
+
+					if(jp_char.test(k) == true){ // Global match
+						matches.push([w, a[key1][0], a[key1][1],a[key1][2]]);
+						console.info(matches);
+						for(key2 in matches){ // Second loop
+
+							if( /n/i.test(k) ){ // Case of "n"
+								n_after = k[k.lastIndexOf( "n" )+1] ;
+								n_before = k[k.lastIndexOf( "n" )-1] ;
+
+								if( (typeof(n_after) !== "undefined") && ( ("n"+n_after) == matches[key2][1] ) ){ //Translate "n+others"
+									tw+=matches[key2][2];
+									$('#trlt_txt').val(tw);
+									// console.error("case : n1");
+								}
+								// if( (typeof(n_after) !== "undefined") && ( ("n"+n_after) != matches[key2][1] ) ){
+								// 	tw+=matches[key2][2]+key; // bug
+								// 	$('#trlt_txt').val(tw);
+								// }
+								if( (typeof(n_before) !== "undefined") && ( /n/i.test(n_before) === true ) ){ // Translae "n"
+									tw+=matches[key2][2];
+									$('#trlt_txt').val(tw);
+								}
+							}
+							else if( ( (jQuery.inArray( key, v ) > -1) && w.length == 1 )  || ( (jQuery.inArray( key, v ) > -1) && (jQuery.inArray( w[idx-1], v ) > -1) ) ){ // Translate directly vowel
+								console.log("no syllable match : vowel : "+matches[key2][1]);
+								tw+=a[key1][1];
+								$('#trlt_txt').val(tw);
+							}
+							else if( (matches[key2][1].length > 1 && /n/i.test(matches[key2][1]) === false) && (/(\w+)/i.exec(k)[0] == matches[key2][1]) ){
+								tw+=a[key1][1];
+								$('#trlt_txt').val(tw);
+							}
+						}
+						console.info("translated word : "+tw);
+						console.log("---");
+					}
+				}
+				if(jQuery.inArray( key, v ) == -1 && /n/i.test(key) === false){ // Tranlate two time consonant (tsu)
+					var idx = w.lastIndexOf(key);
+
+					if( w[idx-1] == key ){
+						tw+=a[19][1]+key; // bug
+						$('#trlt_txt').val(tw);
+					}
+				}
+	}
+}
+
+//--
+
+function clear_translation(){
+$('#trlt_txt').val("");
+$('#trlt_htxt').val("");
+window.location.reload();
+}
+
+function tranlate_char(){
+
+}
+
+function througurl(){
+	var type = $('.l_type_input:checked').val();
+	// console.log(window.history.state);
+	if(!window.history.state){
+		var w = "";
+		var stateObj = { elmt: "" };
+		history.pushState(stateObj, "", "main.php?srch-term=");
+	}
+
+	var stateObj = { elmt: $('#q_txt').val() };
+	var w = $('#q_txt').val().replace(/\s/gi, "+");
+	history.pushState(stateObj, "", "main.php?type="+type+"&srch-term="+w);
+}
